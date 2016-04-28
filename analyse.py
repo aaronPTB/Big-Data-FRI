7322a052b8d7920ae867329d5814a1178832ad11
AaronPTB <aaron.ptb@gmail.com>
2016-04-28 11:52:43 -0500

    update

--- analyse.py
+++ analyse.py
@@ -1,12 +1,6 @@
 import numpy  as np
 import pandas as pd
 
-gene_data = pd.read_csv("rna_tissue.csv",header=0)
-expr_lvl  = gene_data.transpose().ix[5]
+gene_data = pd.read_csv("transcript_rna_tissue.tsv",delimiter="\t",header=0,index_col=0)
 
-expression_types = {"High":3,"Medium":2,"Low":1,"Not detected":0}
-
-for level in expression_types.keys():
-	expr_lvl[expr_lvl==level] = expression_types[level]
-
-print expr_lvl
+print gene_data
